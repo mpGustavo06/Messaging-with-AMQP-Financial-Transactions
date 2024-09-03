da11198de760384cfc5d491d5b5dafd0d4c8f120
Gustavo Matheus Pauvels <myth.gustavo06@gmail.com>
2024-09-03 16:51:48 -0300

    Commit

---
+++ Message-Consumer/src/main/java/br/edu/utfpr/td/tsi/transactions/consumer/MessageConsumerApplication.java
@@ -0,0 +1,13 @@
+package br.edu.utfpr.td.tsi.transactions.consumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessageConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MessageConsumerApplication.class, args);
+	}
+
+}
---
+++ Message-Producer/src/main/java/br/edu/utfpr/td/tsi/transactions/producer/MessageProducerApplication.java
@@ -0,0 +1,13 @@
+package br.edu.utfpr.td.tsi.transactions.producer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessageProducerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MessageProducerApplication.class, args);
+	}
+
+}

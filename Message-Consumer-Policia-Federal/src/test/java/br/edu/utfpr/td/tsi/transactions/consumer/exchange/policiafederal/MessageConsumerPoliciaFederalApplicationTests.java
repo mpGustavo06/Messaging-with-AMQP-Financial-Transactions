c874a163addf605e2d38db12ff2a77e365e1d7c5
Gustavo Matheus Pauvels <myth.gustavo06@gmail.com>
2024-09-04 18:02:35 -0300

    Consumer Policia Federal Version 1.0

---
+++ Message-Consumer-Policia-Federal/src/test/java/br/edu/utfpr/td/tsi/transactions/consumer/exchange/policiafederal/MessageConsumerPoliciaFederalApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.utfpr.td.tsi.transactions.consumer.exchange.policiafederal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessageConsumerPoliciaFederalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
